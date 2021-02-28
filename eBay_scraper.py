25f3727b73fd2443a5389535f8890f6acc037bc1
btnewman7 <btnewman7@gmail.com>
2021-02-28 18:08:47 -0500

    scraper for 200 lines of cards

--- eBay_scraper.py
+++ eBay_scraper.py
@@ -5,16 +5,17 @@
 # Write scraped data to CSV file
 import requests
 import csv
+import re
 from bs4 import BeautifulSoup
 
 
 def get_page(url):
-    response = requests.get(url)
+    response = requests.get(url, timeout=5)
     
     if not response.ok:
         print('Server sponded: ', response.status_code)
     else:
-        soup = BeautifulSoup(response.text, 'html.parser')
+        soup = BeautifulSoup(response.text, 'lxml')
     return soup
 
 def get_detail_data(soup):
@@ -35,20 +36,84 @@ def get_detail_data(soup):
         price = ''
         currency = ''
     
+    #date
+    try:
+        date = soup.find('span', id='bb_tlft').text.strip()
+    except:
+        date = ''
+
     #sport 
     try:
-        rows = soup.find('div', class_='itemAttr').findAll('table')[0].findAll('tr') 
+        item_specs_table = soup.find('div', id="vi-desc-maincntr").find('div', class_="itemAttr").find_all('div', class_="section")[0]
+        sport = item_specs_table.find_all("td", string=re.compile("Sport:"))[0].findNext('td').text.strip()
     except:
-        rows= ''
-    print(rows)
+        sport= ''
 
-    #data = {
-    #    'title': title,
+    #card_manufacturer
+    try:
+        card_manufacturer = item_specs_table.find_all("td", string=re.compile("Card Manufacturer:"))[0].findNext('td').text.strip()
+    except:
+        card_manufacturer= ''
+    
+    #team
+    try:
+        team = item_specs_table.find_all("td", string=re.compile("Team:"))[0].findNext('td').text.strip()
+    except:
+        team= ''
+
+    #player
+    try:
+        player_name = item_specs_table.find_all("td", string=re.compile("Player:"))[0].findNext('td').text.strip()
+    except:
+        player_name= ''
+
+    #year
+    try:
+        year = item_specs_table.find_all("td", string=re.compile("Year:"))[0].findNext('td').text.strip()
+    except:
+        year= ''
+
+    #season_year
+    try:
+        season = item_specs_table.find_all("td", string=re.compile("Season:"))[0].findNext('td').text.strip()
+    except:
+        season= ''
+
+    #grade
+    try:
+        grade = item_specs_table.find_all("td", string=re.compile("Grade:"))[0].findNext('td').text.strip()
+    except:
+        grade= ''
+    
+    #card_attributes
+    try:
+        card_attributes = item_specs_table.find_all("td", string=re.compile("Card Attributes:"))[0].findNext('td').text.strip()
+    except:
+        card_attributes= ''
+
+    #grader
+    try:
+        grader = item_specs_table.find_all("td", string=re.compile("Professional Grader:"))[0].findNext('td').text.strip()
+    except:
+        grader= ''
+
+    data = {
+        'title': title,
+        'player_name': player_name,
     #    'currency': currency,
-    #    'price': price
-    #}
+        'price': price,
+        'date': date,
+        'sport': sport,
+        'team': team,
+        'year': year,
+    #    'season': season,
+        'card_manufacturer': card_manufacturer,
+        'grade': grade,
+        'grader': grader,
+        'card_attributes': card_attributes
+    }
 
-    #return data
+    return data
     
 def get_index_data(soup):
     try:
@@ -60,23 +125,37 @@ def get_index_data(soup):
     
     return urls
 
-#def write_csv(data, url):
-#    with open('output.csv', 'a') as csvfile:
-#        writer = csv.writer(csvfile)
-#        
-#        row = [data['title'],data['currency'], data['price'], url]
-#
-#        writer.writerow(row)
+def write_csv(data, url):
+    with open('output1.csv', 'a') as csvfile:
+        writer = csv.writer(csvfile)
+        
+        row = [data['title'],
+            data['player_name'], 
+        #    data['currency'], 
+            data['price'],
+            data['date'],
+            data['sport'],
+            data['team'],
+            data['year'],
+        #    data['season'],
+            data['card_manufacturer'],
+            data['grade'],
+            data['grader'],
+            data['card_attributes'],
+             url]
+
+        writer.writerow(row)
 
 
 def main():
-    url = 'https://www.ebay.com/itm/1969-TOPPS-BASEBALL-380-STAN-BAHNSEN-EX-MINT-YANKEES/154005822797?hash=item23db765d4d:g:etIAAOSwwOhfC~cu'
-    #url = 'https://www.ebay.com/sch/i.html?_nkw=sports+cards&LH_Sold=1&Product=Single&_pgn=1'
-    get_detail_data(get_page(url))
-    #products = get_index_data(get_page(url))
-    #for link in products:
-        #data = get_detail_data(get_page(link))
-        #write_csv(data, link)
+    #url = 'https://www.ebay.com/itm/1969-TOPPS-BASEBALL-380-STAN-BAHNSEN-EX-MINT-YANKEES/154005822797?hash=item23db765d4d:g:etIAAOSwwOhfC~cu'
+    #get_detail_data(get_page(url))
+    for num in range(1,20):
+        url = f'https://www.ebay.com/sch/i.html?_nkw=sports+cards&LH_Sold=1&Product=Single&_udlo=35&LH_PrefLoc=1&_pgn={num}'
+        products = get_index_data(get_page(url))
+        for link in products:
+            data = get_detail_data(get_page(link))
+            write_csv(data, link)
 
 
 
